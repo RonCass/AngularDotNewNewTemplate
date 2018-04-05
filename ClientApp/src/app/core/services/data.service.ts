d4fb3e359919495e5f74d8ff1dcebf0f04c6451d
RCLumber @ Outlook.com <rclumber@outlook.com>
2018-04-05 07:58:52 -0400

    Added in some data access code that I need to use to build out the data.service.

--- ClientApp/src/app/core/services/data.service.ts
+++ ClientApp/src/app/core/services/data.service.ts
@@ -10,15 +10,17 @@ import { WeatherForecast, Log } from './models';
 import { CurrentUserService } from './current-user.service';
 import { ToastrService } from './toastr.service';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': 'my-auth-token'
-  })
-};
 
 @Injectable()
 export class DataService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.getToken()
+    })
+  };
 
    public baseUrl = 'http://localhost:49223/';
 
@@ -31,19 +33,37 @@ export class DataService {
     return myToken;
   }
 
-  getHeaders() {
+  //getHeaders() {
+
+  //  let headers = new Headers();
+  //  headers.append('Accept', 'application/json');
+  //  headers.append('Content-Type', 'application/json')
+  //  headers.append('Authorization', 'Bearer ' + this.getToken())
+  //  return new RequestOptions({ headers: headers });
+
+  //}
+
+  /////////////////////////////////////////////////////////
+  // Start - Auth Stuff
+  /////////////////////////////////////////////////////////
+
+  createToken(username: string, password: string): Observable<any> {
 
     let headers = new Headers();
     headers.append('Accept', 'application/json');
     headers.append('Content-Type', 'application/json')
-    headers.append('Authorization', 'Bearer ' + this.getToken())
-    return new RequestOptions({ headers: headers });
+    //let options = new RequestOptions({ headers: headers });
+
+    let myJson = JSON.stringify({ 'username': username, 'password': password })
+
+    return this.http.post(this.baseUrl + 'Auth/CreateToken',
+      myJson, this.httpOptions);
 
   }
 
   GetSomething() {
 
-    return this.http.get<WeatherForecast>(this.baseUrl + 'api/SampleData/WeatherForecasts')
+    return this.http.get<WeatherForecast>(this.baseUrl + 'api/SampleData/WeatherForecasts', this.httpOptions)
       .pipe(
       catchError(this.handleError)
       );
@@ -51,7 +71,7 @@ export class DataService {
   }
 
   getLogs() {
-    return this.http.get<Log>(this.baseUrl + 'api/Logs/GetLogs')
+    return this.http.get<Log>(this.baseUrl + 'api/Logs/GetLogs', this.httpOptions)
       .pipe(
       catchError(this.handleError)
       );
@@ -72,5 +92,59 @@ export class DataService {
     return new ErrorObservable(
       'Something bad happened; please try again later.');
   };
+
+  /////////////////////////////////////////////////////////
+  // ERROR HANDLING
+  /////////////////////////////////////////////////////////
+  //private handleError(error: HttpErrorResponse) {
+  //  let errMsg: string;
+
+  //  if (error.error instanceof ErrorEvent) {
+  //    // A client-side or network error occurred. Handle it accordingly.
+  //    console.error('An error occurred:', error.error.message);
+  //    errMsg = "Hit Client Side or Network Error Handler";
+
+  //  } else {
+  //    // The backend returned an unsuccessful response code.
+  //    // The response body may contain clues as to what went wrong,
+  //    if (error.status == 500) { //Internal Server Error
+  //      errMsg = 'Sorry, there was an internal server error. Please hit control-F5 to refresh the page and then try again.';
+  //    }
+  //    else if (error.status == 400) { //Bad Request
+  //      if (error.error) {
+  //        errMsg = 'Error: ' + JSON.stringify(error.error);
+  //      }
+  //      else {
+  //        errMsg = 'Sorry, there was a problem with the data sent to the server (Bad Request Text: ' + error.statusText + '). Please hit control-F5 to refresh the page and then try again.';
+  //      }
+  //    }
+  //    else if (error.status == 401) { //Unauthorized
+  //      if (error.error) {
+  //        errMsg = 'Error: ' + JSON.stringify(error.error);
+  //      }
+  //      else {
+  //        errMsg = 'Your username or passowrd is incorrect. Please check them and try again: ' + error.statusText;
+  //      }
+  //    }
+  //    else {
+  //      if (error.error) {
+  //        let body = error.error;
+  //        const err = body || JSON.stringify(error.error);
+  //        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+  //      }
+  //      else {
+  //        //const body = error.json() || '';
+  //        //const err = body.error || JSON.stringify(body);
+  //        errMsg = `${error.status} - ${error.statusText || ''} `;
+  //        //errMsg = "Got Here in handleObservableError - need to fix this code";
+  //      }
+  //    }
+  //    console.error(
+  //      `Backend returned code ${error.status}, ` +
+  //      `body was: ${error.error}`);
+  //  }
+  //  // return an ErrorObservable with a user-facing error message
+  //  return new ErrorObservable(errMsg);
+  //};
 
 }
