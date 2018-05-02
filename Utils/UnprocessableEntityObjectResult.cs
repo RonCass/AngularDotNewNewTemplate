41ac05ba570682d280bba0ab2f7f7dbd12f4cf5d
RCLumber @ Outlook.com <rclumber@outlook.com>
2018-05-02 08:15:01 -0400

    Added two files

---
+++ Utils/UnprocessableEntityObjectResult.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularDotNetNewTemplate.Utils
+{
+    // 3-24-2017 Ron C.: Created this to return a 422 UnprocessableEntityObject when ModelState Validation fails
+    public class UnprocessableEntityObjectResult : ObjectResult
+    {
+        public UnprocessableEntityObjectResult(ModelStateDictionary modelState)
+            : base(new SerializableError(modelState))
+        {
+            if (modelState == null)
+            {
+                throw new ArgumentNullException(nameof(modelState));
+
+            }
+
+            StatusCode = 422;
+        }
+    }
+}
